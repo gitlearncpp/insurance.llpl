b2b487794bdfdc79dfef73dda299760ec3164669
cisco <subartowicz@gmail.com>
2024-11-25 19:44:19 +0100

    0.1

--- app.py
+++ app.py
@@ -1,63 +1,61 @@
-from flask import Flask, request, render_template, redirect, url_for, flash, jsonify
-
-import sell
+from flask import Flask, request, jsonify
 from house import House
 from person import Person
+from sell import PropertyInsuranceCalculator
 
 app = Flask(__name__)
-app.secret_key = '770-818-154'
 
-@app.route('/person', methods=['POST'])
-def create_person():
-    data = request.json
-    person = Person(
-        first_name=data.get('first_name'),
-        last_name=data.get('last_name'),
-        pesel=data.get('pesel'),
-        street=data.get('street'),
-        city=data.get('city'),
-        number=data.get('number'),
-        postal_code=data.get('postal_code'),
-        phone=data.get('phone'),
-        vip=data.get('vip', False)
-    )
+@app.route("/calculate_price", methods=["POST"])
+def calculate_price():
+    try:
+        data = request.json
 
-    response = {
-      #  'input_data': data,
-        'full_name': person.full_name(),
-        'address': person.adress(),
-        'phone': person.phone,
-        'vip_status': 'VIP' if person.vip else 'Non-VIP'
+        # Pobieranie danych o domu i osobie
+        house_data = data.get("house")
+        person_data = data.get("person")
 
-    }
-    return jsonify(response), 201
+        # Tworzenie obiektu House
+        house = House(
+            price=house_data["price"],
+            bedrooms=house_data["bedrooms"],
+            bathrooms=house_data["bathrooms"],
+            square_feet=house_data["square_feet"],
+            lot_size=house_data["lot_size"],
+            year_built=house_data["year_built"],
+            city=house_data["city"],
+            state=house_data["state"],
+            address=house_data["address"],
+            number=house_data["number"],
+            things_inside=house_data["things_inside"],
+            house_type=house_data.get("house_type", "house")  # Domyślnie "house"
+        )
 
-@app.route('/house', methods=['POST'])
-def create_house():
-    data = request.json
-    house = House(
-        price=data.get('price'),
-        bedrooms=data.get('bedrooms'),
-        bathrooms=data.get('bathrooms'),
-        square_feet=data.get('square_feet'),
-        lot_size=data.get('lot_size'),
-        year_built=data.get('year_built'),
-        city=data.get('city'),
-        state=data.get('state'),
-        address=data.get('address'),
-        number=data.get('number'),
-        things_inside=data.get('things_inside'),
-        house_type=data.get('house_type'),
-    )
+        # Tworzenie obiektu Person (opcjonalne dla tego przykładu)
+        person = Person(
+            first_name=person_data["first_name"],
+            last_name=person_data["last_name"],
+            pesel=person_data["pesel"],
+            street=person_data.get("street"),
+            city=person_data.get("city"),
+            number=person_data.get("number"),
+            postal_code=person_data.get("postal_code"),
+            phone=person_data.get("phone"),
+            vip=person_data.get("vip", False)
+        )
 
-    response = {
-        'complete_adress':house.complete_adress(),
-        'insurance_sum': house.insurance_sum(),
+        # Kalkulacja ceny
+        calculator = PropertyInsuranceCalculator(house)
+        price = calculator.base()
 
-    }
+        return jsonify({
+            "price": price,
+            "house_details": house.complete_adress(),
+            "person_name": person.full_name()
+        }), 200
 
-    return jsonify(response), 201
+    except Exception as e:
+        return jsonify({"error": str(e)}), 400
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     app.run(debug=True)
--- sell.py
+++ sell.py
@@ -1,27 +1,10 @@
 import psycopg2
 from psycopg2 import sql
 
-import sell
-from house import House
-
-
-def create_connection():
-    return psycopg2.connect(
-        dbname="insurance_db",
-        user="cisco",
-        password="Niestety123",
-        host="localhost",
-        port="5432"
-    )
-
-
-conn = create_connection()
-
-# Price:
-
 class PropertyInsuranceCalculator:
     def __init__(self, house):
         self.house = house
+
     def calculate_factor(self):
         if self.house.price <= 200000:
             return 0.95
@@ -32,29 +15,16 @@ class PropertyInsuranceCalculator:
         else:
             return 1.4
 
-
-
-    def age_factor(self):
-        if self.house.age <= 10:
-            return 3.0
-        elif self.house.age <= 20:
-            return 1.3
-        elif self.house.age <= 30:
-            return 1.1
-        elif self.house.age <= 40:
-            return 1.0
-        else:
-            return 0.9
-
-    # Code -> promo code
-    # TODO: create database for promocode (code and discount)
-    #code = False
-    #def code_factor(self):
-     #   if self.house.code == False:
-      #      return 1.0
-       # elif self.house.code == True:
-        #    return 0.9
-
+  #  def age_factor(self):
+   #     if self.house.age <= 10:
+    #        return 3.0
+     #   elif self.house.age <= 20:
+      #      return 1.3
+       ##    return 1.1
+        #elif self.house.age <= 40:
+         #   return 1.0
+      #  else:
+       #     return 0.9
 
     def house_factor(self):
         if self.house.house_type == "house":
@@ -66,6 +36,10 @@ class PropertyInsuranceCalculator:
         elif self.house.house_type == "townhouse":
             return 1.2
 
-
     def base(self):
-        return 500 *self.house_factor() * self.age_factor()  * self.calculate_factor()
+        return (
+            500
+            * self.house_factor()
+         #   * self.age_factor()
+            * self.calculate_factor()
+        )
